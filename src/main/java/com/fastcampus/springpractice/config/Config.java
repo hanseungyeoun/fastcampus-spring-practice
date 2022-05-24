f1cf58d7ae02eaa5ef23d5d2c63699e310f51536
syhan <aksla1122@naver.com>
2022-05-24 23:32:00 +0900

    apply #1 spring controller 추가
    기본 Contrllor 추가함

--- src/main/java/com/fastcampus/springpractice/config/Config.java
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -3,7 +3,9 @@ package com.fastcampus.springpractice.config;
 import com.fastcampus.springpractice.logic.BubbleSort;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan("com.fastcampus.springpractice")
 public class Config {
