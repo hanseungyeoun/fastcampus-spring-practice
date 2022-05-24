b006d0199c806af855b25479387d71c6fff6f575
syhan <aksla1122@naver.com>
2022-05-24 22:23:39 +0900

    Config #1 - spring configuration
    기초 설정 
    루프 패키지 이하에 빈이 있는지 조회한다.

---
+++ src/main/java/com/fastcampus/springpractice/config/Config.java
@@ -0,0 +1,11 @@
+package com.fastcampus.springpractice.config;
+
+import com.fastcampus.springpractice.logic.BubbleSort;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fastcampus.springpractice")
+public class Config {
+
+}
